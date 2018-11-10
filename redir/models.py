7b3a341faeb6732de1fc377347ab73795a5d05c6
Hovhannes Paskhalyan <paskhalyan@gmail.com>
2018-11-10 12:56:59 +0400

    made 'redir' app and ShortURL model in it

---
+++ redir/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class ShortURL(models.Model):
+    url = models.CharField(max_length=220, )
+
+    def __str__(self):
+        return  str(self.url)
+
+    def __unicode__(self):
+        return str(self.url)
